8ea2e876b36727e6270f6e8a5c734a3ca5f30c93
n108qc <140641232+n107qc@users.noreply.github.com>
2024-03-02 20:45:23 +0200

    Add files via upload

--- Shooter_Final_Project/GG.py
+++ Shooter_Final_Project/GG.py
@@ -1,7 +1,9 @@
 from pygame import *
+from pygame.sprite import Sprite
+from pygame.transform import scale, flip
+from pygame.image import load
 from random import randint
-
-import os
+from time import time as timer
 
 init()
 font.init()
@@ -11,21 +13,25 @@ mixer.init()
 WIDTH, HEIGHT = 900, 600
 
 # картинка фону
-bg_image = image.load("infinite_starts.jpg")
+bg= image.load("Img/фон4.jpg")
 #картинки для спрайтів
-player_image = image.load("spaceship.png")
-ufo_image = image.load("alien.png")
+player_img = image.load("Img/спрайт героя.png")
+enemy1_img = image.load("Img/спарайт ворога1.png")
+enemy2_img = image.load("Img/спрайт ворога2.png")
+gun_img = image.load("Img/зброя1-removebg-preview.png")
+gun2_img = image.load("Img/зброя2-removebg-preview.png")
+gun3_img = image.load("Img/зброя3-removebg-preview.png")
+ammo1_img = image.load("Img/куля3-removebg-preview.png")
 
 # фонова музика
-mixer.music.load('musictheme.ogg')
+mixer.music.load('Sound/wildweststinger.wav')
 mixer.music.set_volume(0.2)
-mixer.music.play(-1)
+mixer.music.play(loops=-1)
 
 #окремі звуки
-fire_sound = mixer.Sound("laser1.wav")
+fire_sound = mixer.Sound("Sound/lmg_fire01.mp3")
 fire_sound.set_volume(0.2)
 
-# класи
 class GameSprite(sprite.Sprite):
     def __init__(self, sprite_img, width, height, x, y, speed = 3):
         super().__init__()
@@ -36,18 +42,78 @@ class GameSprite(sprite.Sprite):
         self.speed = speed
         self.mask = mask.from_surface(self.image)  
 
-    def draw(self): #відрисовуємо спрайт у вікні
-        window.blit(self.image, self.rect)
+    def draw(self, surface): #відрисовуємо спрайт у вікні
+        surface.blit(self.image, self.rect)
 
 class Player(GameSprite):
+    def __init__(self, sprite_img, width, height, x, y, speed=3):
+        super().__init__(sprite_img, width, height, x, y, speed)
+        self.arm = gun_img
+        self.arm = transform.scale(gun_img, (90,50))
+
+    def draw(self, surface):
+        super().draw(surface)
+        surface.blit(self.arm, (self.rect.centerx, self.rect.centery))
+
     def update(self): #рух спрайту
+        old_pos = self.rect.x, self.rect.y
         keys_pressed = key.get_pressed() 
-        if keys_pressed[K_LEFT] and self.rect.x > 0:
+        if keys_pressed[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x < WIDTH - 70:
+        if keys_pressed[K_d] and self.rect.x < WIDTH - 70:
             self.rect.x += self.speed
+        if keys_pressed[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < HEIGHT - 70:
+            self.rect.y += self.speed 
         
-class Text(sprite.Sprite):
+        for w in walls:
+            if sprite.collide_mask(self, w):
+                self.rect.x, self.rect.y = old_pos      
+
+    def fire(self):
+        bullet = Bullet(ammo1_img, 15, 20, self.rect.right, self.rect.centery,  10)  
+        bullets.add(bullet) 
+    
+     
+
+class Enemy(GameSprite):
+    def __init__(self, sprite_img, width, height, x, y, speed=3):
+        super().__init__(sprite_img, width, height, x, y, speed)
+        self.direction = 'down'  
+
+    def update(self):
+        if self.direction == 'down':
+            self.rect.y += self.speed
+            if self.rect.bottom >= HEIGHT:  
+                self.rect.y = HEIGHT - self.rect.height  
+                self.direction = 'up'  
+        else:
+            self.rect.y -= self.speed
+            if self.rect.top <= 0:  
+                self.rect.y = 0  
+                self.direction = 'down'  
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.x += self.speed  
+        if self.rect.x > WIDTH:  
+            self.kill()
+
+class Wall(sprite.Sprite):
+    def __init__(self, wall_x, wall_y, wall_width, wall_height):
+        super().__init__()
+        self.width = wall_width
+        self.height = wall_height
+        self.image = Surface([self.width, self.height]) 
+        self.image.fill((152,100,84))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+ 
+
+
+class Text(GameSprite):
     def __init__(self, text, x, y, font_size=22, font_name="Impact", color=(255,255,255)):
         self.font = font.SysFont(font_name, font_size)
         self.image = self.font.render(text, True, color)
@@ -62,31 +128,44 @@ class Text(sprite.Sprite):
     def set_text(self, new_text): #змінюємо текст напису
         self.image = self.font.render(new_text, True, self.color)
 
-
 # створення вікна
 window = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Шутер")
 
 # написи для лічильників очок
-score_text = Text("Рахунок: 0", 20, 50)
+score_text = Text("Рахунок:", 20, 50)
 # напис з результатом гри
 result_text = Text("Перемога!", 350, 250, font_size = 50)
 
 #додавання фону
-bg = transform.scale(bg_image, (WIDTH, HEIGHT))
+bg = transform.scale(bg, (WIDTH, HEIGHT))
 
 # створення спрайтів
-player = Player(player_image, width = 100, height = 100, x = 200, y = HEIGHT-150)
-ufos = sprite.Group() #група спрайтів
+player = Player(player_img, width=300, height=200, x=0, y=415)
+enemy1 = Enemy(enemy1_img, width=239, height=211, x=450, y=225)
+enemy2 = Enemy(enemy2_img, width=200, height=189, x=670, y=15)
 
+enemies = sprite.Group()
+enemies.add(enemy1)
+enemies.add(enemy2)
+
+
+bullets = sprite.Group()
+
+walls = sprite.Group()
+wall1 = Wall(250, 300, 20, 550)
+wall2 = Wall(430, 130, 20, 350)
+wall3 = Wall(680, 450, 20, 180)
+
+walls.add(wall1)
+walls.add(wall2)
+walls.add(wall3)
 
 # основні змінні для гри
 run = True
 finish = False
 clock = time.Clock()
 FPS = 60
-score = 0
-lost = 0
 
 # ігровий цикл
 while run:
@@ -94,32 +173,31 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        if not finish and e.type == KEYDOWN and e.key == K_SPACE:
+            player.fire()
+
     if not finish: # поки гра триває
         # рух спрайтів
         player.update() #рух гравця
-        ufos.update()
 
-         #зіткнення гравця і ворогів
-        spritelist = sprite.spritecollide(player, ufos, False)
+        #зіткнення гравця і ворогів
+        spritelist = sprite.spritecollide(player, enemies, False,sprite.collide_mask)
         for collide in spritelist:
             finish = True
             result_text.set_text("ПРОГРАШ!")
-        # перевірка зіткнення 2 груп спрайтів
-        # spritelist = sprite.groupcollide(ufos, bullets, True, True)
-        # for collide in spritelist:
-        #     explosions.add(Explosion(collide.rect.x, collide.rect.y, images_list))
-        #     score += 1
-        #     score_text.set_text("Рахунок:" + str(score))
-       
-        if score >= 10:
-            finish = True
-         #відрисовуємо фон
+
+        #відрисовуємо фон
         window.blit(bg, (0, 0)) 
         #відрисовуємо спрайти
-        player.draw() 
-        ufos.draw(window)
+        player.draw(window) 
+        enemies.draw(window)
+        walls.draw(window)
+        bullets.draw(window)
+        enemies.update()
+        walls.update()
+        bullets.update()
     else:
-        result_text.draw() # текст вкінці гриfrom pygame import *
-    # оновлення екрану і FPS
+        result_text.draw() 
+
     display.update()
     clock.tick(FPS)
