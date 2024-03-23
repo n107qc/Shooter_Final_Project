0cdaf26af884d08aeffabbfb766a7f63b400f13f
n108qc <140641232+n107qc@users.noreply.github.com>
2024-03-23 20:10:29 +0200

    Add files via upload

--- Shooter_Final_Project/GG.py
+++ Shooter_Final_Project/GG.py
@@ -13,14 +13,18 @@ lost = 0
 score = 0
 
 # розміри вікна
-WIDTH, HEIGHT = 900, 600
+WIDTH, HEIGHT = 1520, 700
 
 # картинка фону
 bg= image.load("Img/фон4.jpg")
+bg2= image.load("Img/фон3.jpg")
+
 #картинки для спрайтів
 player_img = image.load("Img/спрайт героя.png")
 enemy1_img = image.load("Img/спарайт ворога1.png")
 enemy2_img = image.load("Img/спрайт ворога2.png")
+enemy3_img = image.load("Img/спрайт ворога3.png")
+enemy4_img = image.load("Img/спарайт ворога4.png")
 gun_img = image.load("Img/зброя1-removebg-preview.png")
 gun2_img = image.load("Img/зброя2-removebg-preview.png")
 gun3_img = image.load("Img/зброя3-removebg-preview.png")
@@ -75,15 +79,16 @@ class Player(GameSprite):
 
 
     def update(self): #рух спрайту
+        global finish
         old_pos = self.rect.x, self.rect.y
         keys_pressed = key.get_pressed() 
-        if keys_pressed[K_a] and self.rect.x > 0:
+        if keys_pressed[K_a] and self.rect.x > -110:
             self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < WIDTH - 70:
+        if keys_pressed[K_d]: 
             self.rect.x += self.speed
-        if keys_pressed[K_w] and self.rect.y > 0:
+        if keys_pressed[K_w] and self.rect.y > -27:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < HEIGHT - 70:
+        if keys_pressed[K_s] and self.rect.y < HEIGHT - 170:
             self.rect.y += self.speed 
         
         for w in walls:
@@ -92,11 +97,9 @@ class Player(GameSprite):
 
         for w in enemies:
             if sprite.collide_mask(self,w):
-                self.rect.x, self.rect.y = old_pos
+                finish = True
         
-        for w in bullets:
-            if sprite.collide_mask(self,w):
-                self.rect.x, self.rect.y = old_pos
+        
 
     def fire(self):
         # Отримати позицію зброї відносно гравця
@@ -104,7 +107,7 @@ class Player(GameSprite):
         bullet_y = arm_rect.centery-50
         bullet = Bullet(ammo1_img, 40, 45, bullet_x, bullet_y,  10, player.angle, player.dir)  
         bullets.add(bullet)   
-    
+        fire_sound.play()
      
 
 class Enemy(GameSprite):
@@ -142,6 +145,9 @@ class Bullet(GameSprite):
         self.rect.y += self.dir[1] * self.speed  
         if self.rect.x > WIDTH:  
             self.kill()
+        for w in walls:
+            if sprite.collide_mask(self, w):
+                self.kill()        
 
 class Wall(sprite.Sprite):
     def __init__(self, wall_x, wall_y, wall_width, wall_height):
@@ -180,19 +186,25 @@ display.set_caption("Шутер")
 # написи для лічильників очок
 score_text = Text("Рахунок:", 20, 50)
 # напис з результатом гри
-result_text = Text("Перемога!", 350, 250, font_size = 50)
+result_text = Text("Програш", 650, 250, font_size = 100)
+win_text = Text("Перемога", 650, 250, font_size = 100)
 
 #додавання фону
 bg = transform.scale(bg, (WIDTH, HEIGHT))
+bg2 = transform.scale(bg2, (WIDTH, HEIGHT))
 
 # створення спрайтів
 player = Player(player_img, width=300, height=200, x=0, y=415)
 enemy1 = Enemy(enemy1_img, width=200, height=189, x=450, y=225)
 enemy2 = Enemy(enemy2_img, width=180, height=160, x=670, y=15)
+enemy3 = Enemy(enemy1_img, width=200, height=189, x=956, y=425)
+enemy4 = Enemy(enemy2_img, width=180, height=160, x=1350, y=115)
 
 enemies = sprite.Group()
 enemies.add(enemy1)
 enemies.add(enemy2)
+enemies.add(enemy3)
+enemies.add(enemy4)
 
 
 bullets = sprite.Group()
@@ -200,11 +212,15 @@ bullets = sprite.Group()
 walls = sprite.Group()
 wall1 = Wall(250, 300, 20, 550)
 wall2 = Wall(430, 100, 20, 350)
-wall3 = Wall(680, 450, 20, 180)
+wall3 = Wall(945, 150, 20, 380)
+wall4 = Wall(680, 410, 20, 280)
+wall5 = Wall(1360, 150, 20, 380)
 
 walls.add(wall1)
 walls.add(wall2)
 walls.add(wall3)
+walls.add(wall4)
+walls.add(wall5)
 
 # основні змінні для гри
 run = True
@@ -227,13 +243,11 @@ while run:
                     reload = True
                     start_reload = timer()
 
-    if not finish:
-        window.blit(bg, (0, 0))
-        txt_lose = f.render(f'Пропущено:{lost}', True, (255,255,255))
-        txt_score = f.render(f'Рахунок:{score}', True, (255,255,255))
-        window.blit(txt_lose, (0,50))
-        window.blit(txt_score, (0,0))
+    
+    window.blit(bg, (0, 0))
 
+    
+    if not finish:
         mouse_pos = mouse.get_pos()
 
         dx = mouse_pos[0] - player.rect.centerx
@@ -274,24 +288,52 @@ while run:
             if level == 1:
                 level = 2
                 player.rect.x = 0
+                bg=bg2
                 walls = sprite.Group()
                  #якщо були на першому рівні level = 2 # переходимо на 2-й рівень player.rect.x = 0 # гравця перемішаємо вліво walls = sprite.Group() # створюємо нові стіни walls.add ( Wall(250, 300, 20, 550) ) #тут треба вказати стіни для 2 рівня
-                walls. add ( Wall(250, 300, 20, 550) )
-                walls. add ( Wall(250, 300, 20, 550) )  
+                walls.add(Wall(250, 300, 20, 550))
+                walls.add(Wall(550, 50, 20, 370)) 
+                walls.add(Wall(750, 480, 20, 410)) 
+                walls.add(Wall(950, 50, 20, 370)) 
+                walls.add(Wall(1150, 50, 20, 370))
+                walls.add(Wall(1150, 580, 20, 410))
+                walls.add(Wall(750, 50, 20, 200))
 
                 enemies = sprite.Group() # створюємо нових ворогів
-                enemies. add (Enemy (enemy1_img, width=200, height=189, x=450, у=225)) #тут треба вказати ворогів для 2 рівня
-                enemies. add (Enemy(enemy2_img, width=180, height=160, x=670, y=15))
+                enemies.add (Enemy(enemy1_img, width=200, height=189, x=570, y=225)) #тут треба вказати ворогів для 2 рівня
+                enemies.add (Enemy(enemy2_img, width=180, height=160, x=770, y=15))
+                enemies.add (Enemy(enemy3_img, width=180, height=160, x=1320, y=15))
+        if player.rect.right > WIDTH: #якщо торкаємося правого краю вікна
+            if level == 2:
+                level = 3
+                player.rect.x = 0
+                walls = sprite.Group()
+                 #якщо були на першому рівні level = 2 # переходимо на 2-й рівень player.rect.x = 0 # гравця перемішаємо вліво walls = sprite.Group() # створюємо нові стіни walls.add ( Wall(250, 300, 20, 550) ) #тут треба вказати стіни для 2 рівня
+                walls.add(Wall(300, 0, 20, 400))
+                walls.add(Wall(300, 580, 20, 300))
+                walls.add(Wall(500, 0, 20, 550))
+                walls.add(Wall(700, 700, 20, 20))
 
 
+                enemies = sprite.Group() # створюємо нових ворогів
+                enemies.add (Enemy(enemy1_img, width=200, height=189, x=570, y=225)) #тут треба вказати ворогів для 2 рівня
+
+                
+
+
+        enemies.update(lost)
+        walls.update()
+        bullets.update()
     enemies.draw(window)
     walls.draw(window)
     bullets.draw(window)
-    enemies.update(lost)
-    walls.update()
-    bullets.update()
 
-    if score == 2:
+    txt_lose = f.render(f'Пропущено:{lost}', True, (255,255,255))
+    txt_score = f.render(f'Рахунок:{score}', True, (255,255,255))
+    window.blit(txt_lose, (0,50))
+    window.blit(txt_score, (0,0))
+    
+    if score == 0:
         finish = True
     if finish:
         result_text.draw(window) 
